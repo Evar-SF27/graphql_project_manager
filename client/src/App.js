8b2b8ccff78467d2e3027cf46a8a6148ae66b27a
Evar-SF27 <evarfomuso@outlook.com>
2023-10-16 23:09:11 +0100

    client and UI components,  Apollo Client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import './App.css'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+import Header from './components/Header'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
-    <div className="container">
-      <h1>Hello world</h1>
-    </div>
-  );
+    <>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <h1>Hello world</h1>
+        </div>
+      </ApolloProvider>
+    </>
+  )
 }
 
 export default App;
