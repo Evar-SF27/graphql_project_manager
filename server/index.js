e3186a48aa21504998ec33274939031be7d1c549
Evar-SF27 <evarfomuso@outlook.com>
2023-10-16 20:59:46 +0100

    Created client query schema

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const  { graphqlHTTP } = require('express-graphql')
 require('dotenv').config()
 const port = process.env.PORT || 8000
+const schema = require('./schema/schema')
 
 const app = express()
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}))
 
 app.listen(port, console.log(`Console running on port ${port}`))
